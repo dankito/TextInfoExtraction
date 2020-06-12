56373f84d43bf40abbbabf2ab58b5abab9d5ebef
dankito <git@dankito.net>
2020-06-12 17:47:13 +0200

    Using now named capture groups to detect day, month and year

--- src/main/kotlin/net/dankito/text/extraction/info/DateExtractor.kt
+++ src/main/kotlin/net/dankito/text/extraction/info/DateExtractor.kt
@@ -1,7 +1,6 @@
 package net.dankito.text.extraction.info
 
 import net.dankito.text.extraction.info.model.DateData
-import net.dankito.text.extraction.info.model.DatePartsPosition
 import org.slf4j.LoggerFactory
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -10,26 +9,23 @@ import java.util.regex.Pattern
 open class DateExtractor : IDateExtractor {
 
     companion object {
-        const val Day = "([012][0-9]|3[01]|[1-9])"
-        const val Month = "(1[012]|0?[1-9])"
-        const val Year = "(19|20)?\\d\\d"
+        const val Day = "(?<day>[012][0-9]|3[01]|[1-9])"
+        const val Month = "(?<month>1[012]|0?[1-9])"
+        const val Year = "(?<year>(?:19|20)?\\d\\d)"
         const val Separator = "[./ -]"
         const val Boundary = "\\b"
 
         // = "\\b([012][0-9]|3[01]|[1-9])[- /.](1[012]|0?[1-9])[- /.](19|20)?\\d\\d\\b"
         const val DayMonthYearPatternString = "$Boundary$Day$Separator$Month$Separator$Year$Boundary"
         val DayMonthYearPattern = Pattern.compile(DayMonthYearPatternString)
-        val DayMonthYearDatePartsPosition = DatePartsPosition(1, 2, 3)
 
         // = "\\b(1[012]|0?[1-9])[- /.]([012][0-9]|3[01]|[1-9])[- /.](19|20)?\\d\\d\\b"
         const val MonthDayYearPatternString = "$Boundary$Month$Separator$Day$Separator$Year$Boundary"
         val MonthDayYearPattern = Pattern.compile(MonthDayYearPatternString)
-        val MonthDayYearDatePartsPosition = DatePartsPosition(2, 1, 3)
 
         // = "\\b(19|20)?\\d\\d[- /.](1[012]|0?[1-9])[- /.]([012][0-9]|3[01]|[1-9])\\b"
         const val YearMonthDayPatternString = "$Boundary$Year$Separator$Month$Separator$Day$Boundary"
         val YearMonthDayPattern = Pattern.compile(YearMonthDayPatternString)
-        val YearMonthDayDatePartsPosition = DatePartsPosition(3, 2, 1)
 
         // may check also:
         // http://regexlib.com/DisplayPatterns.aspx?cattabindex=4&categoryId=5&AspxAutoDetectCookieSupport=1
@@ -49,11 +45,11 @@ open class DateExtractor : IDateExtractor {
 
     override fun extractDates(lines: List<String>): List<DateData> {
 
-        val dayMonthYearDates = findDateStrings(lines, DayMonthYearPattern, DayMonthYearDatePartsPosition)
+        val dayMonthYearDates = findDateStrings(lines, DayMonthYearPattern)
 
-        val monthDayYearDates = findDateStrings(lines, MonthDayYearPattern, MonthDayYearDatePartsPosition)
+        val monthDayYearDates = findDateStrings(lines, MonthDayYearPattern)
 
-        val yearMonthDayDates = findDateStrings(lines, YearMonthDayPattern, YearMonthDayDatePartsPosition)
+        val yearMonthDayDates = findDateStrings(lines, YearMonthDayPattern)
 
 
         return getLargestList(dayMonthYearDates, monthDayYearDates, yearMonthDayDates)
@@ -77,53 +73,31 @@ open class DateExtractor : IDateExtractor {
         return listOf()
     }
 
-    protected open fun findDateStrings(lines: List<String>, pattern: Pattern, datePartsPosition: DatePartsPosition): List<DateData> {
+    protected open fun findDateStrings(lines: List<String>, pattern: Pattern): List<DateData> {
         val matchers = lines.associateBy( { it } , { pattern.matcher(it) } )
 
         val matches = matchers.filter { it.value.find() }
 
-        val dateStrings = matches.mapValues { findDateStrings(it.value) }
-
-        return mapStringsToDates(dateStrings, datePartsPosition)
+        return matches.map { findDateStrings(it.value, it.key) }.flatten()
     }
 
-    protected open fun findDateStrings(matcher: Matcher): MutableList<String> {
+    protected open fun findDateStrings(matcher: Matcher, line: String): List<DateData> {
         matcher.reset()
-        val matches = mutableListOf<String>()
+        val matches = mutableListOf<DateData>()
 
         while (matcher.find()) {
-            matches.add(matcher.group())
+            try {
+                val day = matcher.group("day").toInt()
+                val month = matcher.group("month").toInt()
+                val year = matcher.group("year").toInt()
+
+                matches.add(DateData(day, month, year, matcher.group(), line))
+            } catch (e: Exception) {
+                log.error("Could not map date string '${matcher.group()}' in line '$line' to Date", e)
+            }
         }
 
         return matches
-    }
-
-    protected open fun mapStringsToDates(dateStrings: Map<String, List<String>>, datePartsPosition: DatePartsPosition): List<DateData> {
-        return dateStrings.map { mapStringsToDates(it.key, it.value, datePartsPosition) }.flatten()
-    }
-
-    protected open fun mapStringsToDates(line: String, dateStrings: List<String>, datePartsPosition: DatePartsPosition): List<DateData> {
-        return dateStrings.mapNotNull { mapStringToDate(line, it, datePartsPosition) }
-    }
-
-    protected open fun mapStringToDate(line: String, dateString: String, datePartsPosition: DatePartsPosition): DateData? {
-        try {
-            val dateStringWithNormalizedSeparator = dateString.replace(".", " ").replace("/", " ").replace("-", " ")
-
-            val dateParts = dateStringWithNormalizedSeparator.split(" ")
-
-            if (dateParts.size == 3) {
-                return DateData(dateParts[datePartsPosition.dayPosition - 1].toInt(),
-                    dateParts[datePartsPosition.monthPosition - 1].toInt(),
-                    dateParts[datePartsPosition.yearPosition - 1].toInt(),
-                    dateString,
-                    line)
-            }
-        } catch (e: Exception) {
-            log.error("Could not map date string $dateString to Date", e)
-        }
-
-        return null
     }
 
 }
